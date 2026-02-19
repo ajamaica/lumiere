7b1163971f5e4f414c8f94002ca2bd0c36dfd4f8
Arturo Jamaica <>
2026-02-19 10:41:23 -0600

    formated

--- targets/widget/expo-target.config.js
+++ targets/widget/expo-target.config.js
@@ -1,10 +1,10 @@
 /** @type {import('@bacons/apple-targets').Config} */
 module.exports = {
-  type: "widget",
-  name: "LumiereWidget",
-  bundleIdentifier: "bot.lumiere.app.widget",
-  deploymentTarget: "17.0",
+  type: 'widget',
+  name: 'LumiereWidget',
+  bundleIdentifier: 'bot.lumiere.app.widget',
+  deploymentTarget: '17.0',
   entitlements: {
-    "com.apple.security.application-groups": ["group.bot.lumiere.app"],
+    'com.apple.security.application-groups': ['group.bot.lumiere.app'],
   },
 }
